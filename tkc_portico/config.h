b2961b9aff4903d179b9e86e69902ebfe36c7ec4
Wilson Lam <wilson.j.lam@gmail.com>
2021-12-25 17:18:02 -0800

    move files over from qmk to a dedicated repo

---
+++ tkc_portico/config.h
@@ -0,0 +1,23 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#undef TAPPING_TERM
+#define TAPPING_TERM 121
+
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL 15
+
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED 9
+
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX 30
+
+#undef MOUSEKEY_WHEEL_MAX_SPEED
+#define MOUSEKEY_WHEEL_MAX_SPEED 1
+
+#undef MOUSEKEY_WHEEL_TIME_TO_MAX
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 80
+
